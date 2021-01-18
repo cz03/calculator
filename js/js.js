983a7310e222f265ef9764d2e32ae8a619043f36
cz03 <58660877+cz03@users.noreply.github.com>
2021-01-18 07:37:17 +0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,6 +1,5 @@
 
 
 $(document).on('input', $('input'), function() {
-	console.log('+');
 	$('input')[3].value = $('input')[0].value * ($('input')[1].value * 200 + Number($('input')[2].value));
 });

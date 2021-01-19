2f5775ca5491b352807cc32ccbb8084fce1b06b9
cz03 <58660877+cz03@users.noreply.github.com>
2021-01-19 06:36:26 +0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,5 +1,14 @@
 
 
 $(document).on('input', $('input'), function() {
-	$('input')[3].value = $('input')[0].value * ($('input')[1].value * 200 + Number($('input')[2].value));
+	$('input')[3].value = $('input')[0].value * ($('input')[1].value * $('select option:selected').val() + Number($('input')[2].value));
 });
+
+$(document).on('change', $('select'), function() {
+	if ($('select option:selected').text() === 'Полимочевина') {
+		$('.thick').text('Толщина в ММ');
+	} else {
+		$('.thick').text('Толщина в СМ');
+	}
+
+})

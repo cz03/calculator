fa4f104ef08e3c1721501c51089684ebb5711bbb
cz03 <58660877+cz03@users.noreply.github.com>
2021-01-19 07:09:20 +0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -4,8 +4,8 @@ $('.calc').on('input', $('input'), function() {
 	$('input')[3].value = $('input')[0].value * ($('input')[1].value * $('select option:selected').val() + Number($('input')[2].value));
 });
 
-$('.calc').on('click', $('select'), function() {
-	if ($('select option:selected').text() === 'Полимочевина') {
+$('.calc').on('change', $('select'), function() {
+	if ($('select option:selected').text() == 'Полимочевина') {
 		$('.thick').text('Толщина в ММ');
 	} else {
 		$('.thick').text('Толщина в СМ');
